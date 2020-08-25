a68d368620cdbbfbdd963978e5e6757c2e8864bf
sapphi-red <green@sapphi.red>
2020-08-25 23:03:34 +0900

    mul_idx_status_category_id_created_at => mul_idx_category_id_created_at

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -35,7 +35,7 @@ CREATE TABLE `items` (
   INDEX idx_created_at (`created_at`),
   INDEX mul_idx_seller_id_created_at (`seller_id`, `created_at`),
   INDEX mul_idx_buyer_id_created_at (`buyer_id`, `created_at`),
-  INDEX mul_idx_status_category_id_created_at (`status`, `category_id`, `created_at`)
+  INDEX mul_idx_category_id_created_at (`category_id`, `created_at`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `transaction_evidences`;
