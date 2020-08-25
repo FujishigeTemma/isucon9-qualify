f4265ffab2ee77e7c9e31e89f8dab7edd1a58a44
FUJISHIGE TEMMA <tenma.x0@gmail.com>
2020-08-26 02:19:15 +0900

    wiretap passwords

--- go/sub/login.go
+++ go/sub/login.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 
@@ -32,18 +33,19 @@ func (s *CacheMap) Load(key string) (string, bool) {
 }
 
 var (
-	dbx   *sqlx.DB
-	cache CacheMap
+	dbx             *sqlx.DB
+	cache           CacheMap
+	userPasswordMap map[string]string
 )
 
 func main() {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true&loc=Local",
-		"isucari",   // user
-		"isucari",   // password
+		"isucari",      // user
+		"isucari",      // password
 		"172.16.0.162", // host
-		"3306",      // port
-		"isucari",   // dbname
+		"3306",         // port
+		"isucari",      // dbname
 	)
 
 	_dbx, err := sqlx.Open("mysql", dsn)
@@ -52,6 +54,22 @@ func main() {
 	}
 	dbx = _dbx
 	defer dbx.Close()
+
+	var defaultUsers []struct {
+		Name string `db: account_name`
+	}
+	if err := dbx.Select(&defaultUsers, "SELECT account_name from users"); err != nil {
+		log.Fatalf("failed to get defaultUsers: %s.", err.Error())
+	}
+	for _, u := range defaultUsers {
+		userPasswordMap[u.Name] = ""
+	}
+	defer func() {
+		_, err := flushPasswordData()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
 
 	cache = CacheMap{}
 
@@ -91,6 +109,10 @@ func auth(w http.ResponseWriter, r *http.Request) {
 		outputErrorMsg(w, http.StatusBadRequest, "all parameters are required")
 
 		return
+	}
+
+	if _, ok := userPasswordMap[accountName]; ok {
+		userPasswordMap[accountName] = password
 	}
 
 	u := User{}
@@ -152,4 +174,15 @@ func pollDB(dbx *sqlx.DB) {
 		log.Println("ping pong")
 		time.Sleep(time.Second)
 	}
+}
+
+func flushPasswordData() (int, error) {
+	file, err := os.OpenFile("passwords.txt", os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		//エラー処理
+		log.Fatal(err)
+	}
+	defer file.Close()
+	bytes, _ := json.Marshal(userPasswordMap)
+	return file.Write(bytes)
 }
