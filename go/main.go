eaa5d9570768c84cb4d3ccbc3e07b3bcb8ad900c
sapphi-red <green@sapphi.red>
2020-08-26 03:33:53 +0900

    bumpCharge 3s => 1s

--- go/main.go
+++ go/main.go
@@ -56,7 +56,7 @@ const (
 	ShippingsStatusShipping   = "shipping"
 	ShippingsStatusDone       = "done"
 
-	BumpChargeSeconds = 3 * time.Second
+	BumpChargeSeconds = 1 * time.Second
 
 	ItemsPerPage        = 48
 	TransactionsPerPage = 10
