3068958741c117b74cbbdbb0db3b129111c03446
sapphi-red <green@sapphi.red>
2020-08-26 00:39:46 +0900

    Revert "fix"
    This reverts commit 973ad3728e7b43d88ae07fbec3237509a5d92026.

--- go/main.go
+++ go/main.go
@@ -554,16 +554,14 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := dbx.Select(&items,
-			"(SELECT * FROM `items` WHERE `status` IN (?,?) AND `created_at` = ? AND `id` < ? ORDER BY `id` DESC LIMIT ?) UNION ALL (SELECT * FROM `items` WHERE `status` IN (?,?) AND `created_at` < ? ORDER BY `created_at` DESC LIMIT ?) LIMIT ?",
+			"(SELECT * FROM `items` WHERE `status` IN (?,?) AND `created_at` = ? AND `id` < ? ORDER BY `id` DESC) UNION ALL (SELECT * FROM `items` WHERE `status` IN (?,?) AND `created_at` < ? ORDER BY `created_at` DESC) LIMIT ?",
 			ItemStatusOnSale,
 			ItemStatusSoldOut,
 			time.Unix(createdAt, 0),
-			ItemsPerPage+1,
 			itemID,
 			ItemStatusOnSale,
 			ItemStatusSoldOut,
 			time.Unix(createdAt, 0),
-			ItemsPerPage+1,
 			ItemsPerPage+1,
 		)
 		if err != nil {
@@ -1110,7 +1108,7 @@ func getTransactions(w http.ResponseWriter, r *http.Request) {
 	if itemID > 0 && createdAt > 0 {
 		// paging
 		err := tx.Select(&items,
-			"(SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?,?,?,?,?) AND `created_at` = ? AND `id` < ? ORDER BY `id` DESC) UNION ALL (SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?,?,?,?,?) AND `created_at` < ? ORDER BY `created_at` DESC) LIMIT ?",
+			"(SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?,?,?,?,?) AND `created_at` = ? AND `id` < ? ORDER BY `id` DESC) UNION ALL (SELECT * FROM `items` WHERE (`seller_id` = ? OR `buyer_id` = ?) AND `status` IN (?,?,?,?,?) AND `created_at` < ? ORDER BY `created_at` DESC) DESC LIMIT ?",
 			userId,
 			userId,
 			ItemStatusOnSale,
