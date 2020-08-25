c9eddc701cb82fd26a3d5174e36c9bbac1e7f0e8
sapphi-red <green@sapphi.red>
2020-08-25 17:33:55 +0900

    fix

--- go/main.go
+++ go/main.go
@@ -569,7 +569,7 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 		// paging
 		if len(categories) > 0 {
 			inQuery, inArgs, err = sqlx.In(
-				"SELECT * FROM `items` WHERE `status` = (?,?) AND category_id IN (?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+				"SELECT * FROM `items` WHERE `status` IN (?,?) AND category_id IN (?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 				ItemStatusOnSale,
 				ItemStatusSoldOut,
 				categories,
@@ -580,7 +580,7 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 			)
 		} else {
 			inQuery, inArgs, err = sqlx.In(
-				"SELECT * FROM `items` WHERE `status` = (?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+				"SELECT * FROM `items` WHERE `status` IN (?,?) AND (`created_at` < ?  OR (`created_at` <= ? AND `id` < ?)) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 				ItemStatusOnSale,
 				ItemStatusSoldOut,
 				time.Unix(createdAt, 0),
@@ -593,7 +593,7 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 		// 1st page
 		if len(categories) > 0 {
 			inQuery, inArgs, err = sqlx.In(
-				"SELECT * FROM `items` WHERE `status` = (?,?) AND category_id IN (?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+				"SELECT * FROM `items` WHERE `status` IN (?,?) AND category_id IN (?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 				ItemStatusOnSale,
 				ItemStatusSoldOut,
 				categories,
@@ -601,7 +601,7 @@ func getNewItems(w http.ResponseWriter, r *http.Request) {
 			)
 		} else {
 			inQuery, inArgs, err = sqlx.In(
-				"SELECT * FROM `items` WHERE `status` = (?,?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
+				"SELECT * FROM `items` WHERE `status` IN (?,?) ORDER BY `created_at` DESC, `id` DESC LIMIT ?",
 				ItemStatusOnSale,
 				ItemStatusSoldOut,
 				ItemsPerPage+1,
