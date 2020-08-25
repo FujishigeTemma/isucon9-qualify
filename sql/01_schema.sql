73d40bf8d33fa4d3bc42d97a2b88fffa23cef294
sapphi-red <green@sapphi.red>
2020-08-25 23:33:36 +0900

    set UNIQUE to idx_account_name

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE `users` (
   `num_sell_items` int unsigned NOT NULL DEFAULT 0,
   `last_bump` datetime NOT NULL DEFAULT '2000-01-01 00:00:00',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  INDEX idx_account_name (`account_name`)
+  UNIQUE INDEX idx_account_name (`account_name`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4;
 
 DROP TABLE IF EXISTS `items`;
