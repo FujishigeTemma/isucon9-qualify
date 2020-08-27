c67423dc4133bde54cae2055f223bae01a3900b0
sapphi-red <green@sapphi.red>
2020-08-27 16:37:35 +0900

    improve getImageURL

--- go/main.go
+++ go/main.go
@@ -2509,5 +2509,5 @@ func outputErrorMsg(w http.ResponseWriter, status int, msg string) {
 }
 
 func getImageURL(imageName string) string {
-	return fmt.Sprintf("/upload/%s", imageName)
+	return "/upload/" + imageName
 }
