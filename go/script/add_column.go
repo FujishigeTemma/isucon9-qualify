41aa00d539d10c323259eb99de1874eb314bab9c
ryoha000 <48485650+ryoha000@users.noreply.github.com>
2020-08-31 00:35:25 +0900

    fix changeDBSchema

--- go/script/add_column.go
+++ go/script/add_column.go
@@ -35,22 +35,74 @@ func main() {
 const prefixBeforeAddParentCategoryIds = "INSERT INTO `items` (`id`,`seller_id`,`buyer_id`,`status`,`name`,`price`,`description`,`image_name`,`category_id`,`created_at`,`updated_at`) VALUES "
 const prefixAfterAddParentCategoryIds = "INSERT INTO `items` (`id`,`seller_id`,`buyer_id`,`status`,`name`,`price`,`description`,`image_name`,`category_id`,`parent_category_id`,`created_at`,`updated_at`) VALUES "
 
-const prefixBeforeRemoveTransactionEvidenceColumns = "INSERT INTO `transaction_evidences` (`seller_id`, `buyer_id`, `status`, `item_id`, `item_name`, `item_price`, `item_description`,`item_category_id`,`item_root_category_id`) VALUES "
+const prefixBeforeRemoveTransactionEvidenceColumns = "INSERT INTO `transaction_evidences` (`seller_id`, `buyer_id`, `status`, `item_id`, `item_name`, `item_price`, `item_description`, `item_category_id`, `item_root_category_id`) VALUES "
 const prefixAfterRemoveTransactionEvidenceColumns = "INSERT INTO `transaction_evidences` (`seller_id`, `buyer_id`, `status`, `item_id`) VALUES "
 
 const prefixBeforeRemoveShippingColumns = "INSERT INTO `shippings` (`transaction_evidence_id`, `status`, `item_name`, `item_id`, `reserve_id`, `reserve_time`, `to_address`, `to_name`, `from_address`, `from_name`, `img_binary`) VALUES "
 const prefixAfterRemoveShippingColumns = "INSERT INTO `shippings` (`transaction_evidence_id`, `status`, `reserve_id`, `reserve_time`, `img_binary`) VALUES "
 
+type SchemaKey struct {
+	before int
+	after int
+}
+type SchemaValue struct {
+	value string
+	
+}
+
 func changeDBSchema(line, prefixBefore, prefixAfter string) string {
 	line = strings.TrimPrefix(line, prefixBefore)
 	itemStrings := strings.SplitAfter(line, "), ")
 
+	beforeKeys := getKeys(prefixBefore)
+	afterKeys := getKeys(prefixAfter)
+
+	keyIndexes := []SchemaKey{}
+	for beforeIndex, beforeKey := range beforeKeys {
+		for afterIndex, afterKey := range afterKeys {
+			if afterKey == beforeKey {
+				keyIndexes = append(keyIndexes, SchemaKey{
+					before: beforeIndex,
+					after: afterIndex,
+				})
+				break
+			}
+		}
+	}
+
 	var builder strings.Builder
 	builder.WriteString(prefixAfter)
-	for i := range itemStrings {
-		builder.WriteString(addParentCategoryId(itemStrings[i]))
+
+	itemLen := len(itemStrings)
+	for i, str := range itemStrings {
+		values := getKeys(str)
+		afterValue := make([]string, len(keyIndexes))
+		for valueIndex, value := range values {
+			for _, keyIndex := range keyIndexes {
+				if valueIndex == keyIndex.before {
+					afterValue[keyIndex.after] = value
+					break
+				}
+			}
+		}
+		builder.WriteString("(")
+		builder.WriteString(strings.Join(afterValue, ", "))
+		if i + 1 == itemLen {
+			builder.WriteString(")")
+		} else {
+			builder.WriteString("), ")
+		}
 	}
+
 	return builder.String()
+}
+
+func getKeys(str string) []string {
+	return strings.Split(getKakkoContent(strings.ReplaceAll(str, " ", "")), ",")
+}
+
+func getKakkoContent(str string) string {
+	return strings.Split(strings.Split(str, "(")[1], ")")[0]
 }
 
 var categoryRe = regexp.MustCompile(`\.jpg', (\d+), `)
